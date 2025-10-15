6677dad612fe0f829f31f8c0da17d29616013311
ChinaiArman <chinaiarman@gmail.com>
2025-10-15 03:23:44 -0700

    Make pathfinding more resilient

--- Assets/MonsterAI/Pathfinding.cs
+++ Assets/MonsterAI/Pathfinding.cs
@@ -6,19 +6,23 @@ public class AgentController : MonoBehaviour
 {
     [SerializeField] private Transform goal;
     [SerializeField] private float repathRate = 3f;
-    [SerializeField] private float searchRadius = 10f;
+    [SerializeField] private float minSearchRadius = 10f;
     [SerializeField] private float destinationTolerance = 1f;
     [SerializeField] private float arrivalThreshold = 2f;
+    [SerializeField] private float maxSearchRadius = 20f;
 
     private NavMeshAgent agent;
+    private float effectiveSearchRadius;
     private float repathTimer;
     private Vector3 lastDestination;
+    private int failedSamples = 0;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         agent.autoRepath = true;
         lastDestination = transform.position;
+        effectiveSearchRadius = minSearchRadius;
     }
 
     void Update()
@@ -34,7 +38,7 @@ public class AgentController : MonoBehaviour
 
         if (distToGoal <= arrivalThreshold && goalReachable)
         {
-            if (NavMesh.SamplePosition(goal.position, out NavMeshHit goalHit, searchRadius, NavMesh.AllAreas))
+            if (NavMesh.SamplePosition(goal.position, out NavMeshHit goalHit, minSearchRadius, NavMesh.AllAreas))
             {
                 agent.SetDestination(goalHit.position);
             }
@@ -56,15 +60,39 @@ public class AgentController : MonoBehaviour
     void UpdateDestination()
     {
         Vector3 dirToGoal = (goal.position - transform.position).normalized;
-        Vector3 probePoint = transform.position + dirToGoal * searchRadius;
+        Vector3 probePoint = transform.position + dirToGoal * effectiveSearchRadius;
 
-        if (NavMesh.SamplePosition(probePoint, out NavMeshHit hit, searchRadius, NavMesh.AllAreas))
+        if (NavMesh.SamplePosition(probePoint, out NavMeshHit hit, effectiveSearchRadius, NavMesh.AllAreas))
         {
+            failedSamples = 0;
+            effectiveSearchRadius = minSearchRadius;
+
             if (Vector3.Distance(hit.position, lastDestination) > destinationTolerance)
             {
                 agent.SetDestination(hit.position);
                 lastDestination = hit.position;
                 agent.isStopped = false;
+            }
+        }
+        else
+        {
+            failedSamples++;
+            effectiveSearchRadius = Mathf.Min(minSearchRadius + failedSamples, maxSearchRadius);
+
+            NavMeshPath tempPath = new NavMeshPath();
+            if (NavMesh.CalculatePath(transform.position, goal.position, NavMesh.AllAreas, tempPath)
+                && tempPath.status != NavMeshPathStatus.PathInvalid
+                && tempPath.corners.Length > 1)
+            {
+                Vector3 partialDest = tempPath.corners[1];
+                Vector3 smoothDest = Vector3.Lerp(lastDestination, partialDest, 0.2f);
+
+                if (Vector3.Distance(smoothDest, lastDestination) > destinationTolerance)
+                {
+                    agent.SetDestination(smoothDest);
+                    lastDestination = smoothDest;
+                    agent.isStopped = false;
+                }
             }
         }
     }
@@ -75,9 +103,13 @@ public class AgentController : MonoBehaviour
         {
             Gizmos.color = Color.green;
             Gizmos.DrawLine(transform.position, goal.position);
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawWireSphere(goal.position, arrivalThreshold);
         }
 
         Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, searchRadius);
+        Gizmos.DrawWireSphere(transform.position, effectiveSearchRadius);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(lastDestination, 0.5f);
     }
 }
