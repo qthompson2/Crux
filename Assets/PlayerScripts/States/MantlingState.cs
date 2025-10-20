034a8dd61f9d74803136d65d950d2b6d0ca30ef5
ChinaiArman <chinaiarman@gmail.com>
2025-10-20 00:49:36 -0700

    Remove if statement

--- Assets/PlayerScripts/States/MantlingState.cs
+++ Assets/PlayerScripts/States/MantlingState.cs
@@ -7,22 +7,19 @@ public class MantlingState : BaseState
         // Mantle Logic
 
         // ---- Handle Transitions ----
-        if (t >= 1f)
+        Vector2 input = player.inputHandler.MoveInput;
+        if (input.magnitude > 0.1f)
         {
-            Vector2 input = player.inputHandler.MoveInput;
-            if (input.magnitude > 0.1f)
-            {
-                if (player.inputHandler.SprintHeld)
-                    player.SwitchState(player.sprintingState);
-                else if (player.inputHandler.CrouchHeld)
-                    player.SwitchState(player.crouchingState);
-                else
-                    player.SwitchState(player.walkingState);
-            }
+            if (player.inputHandler.SprintHeld)
+                player.SwitchState(player.sprintingState);
+            else if (player.inputHandler.CrouchHeld)
+                player.SwitchState(player.crouchingState);
             else
-            {
-                player.SwitchState(player.idleState);
-            }
+                player.SwitchState(player.walkingState);
+        }
+        else
+        {
+            player.SwitchState(player.idleState);
         }
     }
 }
