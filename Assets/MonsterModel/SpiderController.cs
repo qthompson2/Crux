4686b8cbc398cbb7252e94508b824363034b92c2
ChinaiArman <chinaiarman@gmail.com>
2025-11-17 21:13:51 -0800

    Fix stick to walls

--- Assets/MonsterModel/SpiderController.cs
+++ Assets/MonsterModel/SpiderController.cs
@@ -38,9 +38,18 @@ public class SpiderController : MonoBehaviour
 		{
             alignAllowed = true;
 		}
+        Debug.Log("Direction Magnitude: " + math.clamp(math.abs(direction.x + direction.y + direction.z), 0, 1));
         animator.SetFloat("Speed", math.clamp(math.abs(direction.x + direction.y + direction.z), 0, 1));
 
         rigidbody.AddForce(gravityModifier * 9.8f * Vector3.down, ForceMode.Acceleration);
+
+        if (distance > 10f)
+        {
+            DisableColliderTemporarily(GetComponent<BoxCollider>(), GetComponent<CapsuleCollider>());
+        } else
+        {
+            EnableCollider(GetComponent<BoxCollider>(), GetComponent<CapsuleCollider>());
+        }
     }
 
     void OnCollisionStay(Collision collision)
@@ -61,4 +70,15 @@ public class SpiderController : MonoBehaviour
 		}
     }
 
+    private void DisableColliderTemporarily(Collider BoxCollider, Collider CapsuleCollider)
+    {
+        BoxCollider.enabled = false;
+        CapsuleCollider.enabled = false;
+    }
+
+    private void EnableCollider(Collider BoxCollider, Collider CapsuleCollider)
+    {
+        BoxCollider.enabled = true;
+        CapsuleCollider.enabled = true;
+    }
 }
